56ffd33ea7a345704a9ff024976e7910ac8b99aa
moutia <medmoutiabenhtira1920@gmail.com>
2025-02-24 09:42:34 +0100

    corriger route de getall pour l'admin

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'user']);
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/users', [AuthController::class, 'getAllUsers']);
+   
   
 
     Route::middleware('role:admin')->group(function () {
