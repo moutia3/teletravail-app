7120e37d925c5e97bef3041b0f3509a200250664
nizar <dabboussinzr@gmail.com>
2025-03-03 12:12:15 -0800

    modifier nom de register

--- back/app/Repositories/AuthRepositoryInterface.php
+++ back/app/Repositories/AuthRepositoryInterface.php
@@ -4,7 +4,7 @@ namespace App\Repositories;
 
 interface AuthRepositoryInterface
 {
-    public function register(array $data);
+    public function addUser(array $data);
     public function login(array $credentials);
     public function logout($user);
     public function getUser($user);
